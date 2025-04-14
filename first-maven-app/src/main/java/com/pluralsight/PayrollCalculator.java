99803bf4b66d55d1fba334a9ee891d459811bab3
Amari White <amariwhite25@gmail.com>
2025-04-14 14:06:51 -0400

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ first-maven-app/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter you name");
+        String name = keyboard.nextLine();
+
+        System.out.println("Enter Hours Worked");
+        float hoursWorked = keyboard.nextFloat();
+
+        System.out.println("Enter you pay rate");
+        float payRate = keyboard.nextFloat();
+
+        float grossPay = hoursWorked * payRate;
+       System.out.printf("Employee Name: %s %n ", name);
+       System.out.printf("Gross pay %.2f", grossPay);
+    }
+}
